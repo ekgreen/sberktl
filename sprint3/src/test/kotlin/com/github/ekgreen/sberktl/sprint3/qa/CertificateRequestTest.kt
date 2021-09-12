074d0a45a4a55c275ef7d52bcfa18da4780234ea
Elion Green <rgizmalkov.dev@gmail.com>
2021-09-12 18:02:50 +0300

    Забыл unmock

--- sprint3/src/test/kotlin/com/github/ekgreen/sberktl/sprint3/qa/CertificateRequestTest.kt
+++ sprint3/src/test/kotlin/com/github/ekgreen/sberktl/sprint3/qa/CertificateRequestTest.kt
@@ -2,12 +2,16 @@ package com.github.ekgreen.sberktl.sprint3.qa
 
 import io.mockk.every
 import io.mockk.mockkObject
+import io.mockk.unmockkAll
+import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
 import ru.sber.qa.CertificateRequest
 import ru.sber.qa.CertificateType
 import ru.sber.qa.Scanner
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 internal class CertificateRequestTest {
 
     @Test
@@ -30,5 +34,10 @@ internal class CertificateRequestTest {
 
     companion object {
         const val EMPLOYEE_NUMBER: Long = 1L
+    }
+
+    @AfterAll
+    fun afterTest(){
+        unmockkAll()
     }
 }
